d9101382a41333998719ee9059bb4d91782d1390
manualba <45296827+manualba@users.noreply.github.com>
2019-05-16 19:38:34 +0200

    Commit
    Terminado

---
+++ grafico/prueba.java
@@ -0,0 +1,30 @@
+package grafico;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class prueba {
+
+	public static void guardar(String usuario) throws IOException {
+		
+		FileWriter writer= new FileWriter("src/Proyecto/RecordarUsuario");
+		writer.write(usuario);
+		writer.close();
+
+
+	}
+	
+	
+	public static String recordar() throws IOException {
+		String text= "";
+		
+		String ruta = "src/Proyecto/RecordarUsuario";
+		File fichero = new File(ruta);
+		Scanner sc = new Scanner(fichero);
+		return text = sc.nextLine();
+		
+	}
+	
+}
